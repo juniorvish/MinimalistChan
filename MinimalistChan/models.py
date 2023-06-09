fe430453c75e9b2682c7645c1a807c4957c04f34
builderbot <135554020+juniorvish@users.noreply.github.com>
2023-06-09 18:40:20 +0530

    Create MinimalistChan/models.py

---
+++ MinimalistChan/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    replies = db.relationship('Reply', backref='post', lazy=True)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
+
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Reply {self.id}>'
